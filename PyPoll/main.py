c627dfb1816cdb5e63fcdd582d2f96d8cea75b9a
elfulenw <elfulenw@gmail.com>
2020-07-06 22:41:09 -0400

    Analyzed PyPoll data

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,15 +2,12 @@ import csv, os
 
 file = os.path.join('Resources', 'election_data.csv')
 
-votes = []
 totalVotes = 0
 county = []
 candidates = []
+candidate_votes = {}
 candidate = 0
-Khan = []
-Correy = []
-Li = []
-Otooley = []
+winner = ['',0]
 
 with open(file) as readfile:
     csvreader = csv.reader(readfile)
@@ -19,56 +16,76 @@ with open(file) as readfile:
     next(csvreader, None)
 
     for row in csvreader:
-        totalVotes = totalVotes + 1
-        votes.append(row[0])
-        candidates.append(row[2])
+        totalVotes += 1
+
+
+        # candidates.append(row[2])
+
+        candidate = row[2]
+
+        if candidate not in candidates:
+            candidates.append(candidate)
+            candidate_votes[candidate] = 0
+        
+        candidate_votes[candidate] += 1
     
+    for candidate in candidate_votes:
+        votes = candidate_votes.get(candidate)
+        # print(f'{candidate}: {(votes/totalVotes)*100:.3f}% ({votes})')
+
+        if votes > int(winner[1]):
+            winner[0] = candidate
+            winner[1] = votes
+    
+    # print(winner[0])
+
     #total vote count
     # totalVotes = (len(votes))
-    print(totalVotes)
+    # print(totalVotes)
 
-    #votes by candidate
-    for candidate in candidates:
-        if candidate == "Khan":
-            Khan.append(candidates)
-            KhanVotes = len(Khan)
-        elif candidate == "Correy":
-            Correy.append(candidates)
-            CorreyVotes = len(Correy)
-        elif candidate == "Li":
-            Li.append(candidates)
-            LiVotes = len(Li)
-        else:
-            Otooley.append(candidates)
-            OtooleyVotes = len(Otooley)
-    print(KhanVotes)
-    print(CorreyVotes)
-    print(LiVotes)
-    print(OtooleyVotes)
+    # #votes by candidate
+    # for candidate in candidates:
+    #     if candidate == "Khan":
+    #         Khan.append(candidates)
+    #         KhanVotes = len(Khan)
+    #     elif candidate == "Correy":
+    #         Correy.append(candidates)
+    #         CorreyVotes = len(Correy)
+    #     elif candidate == "Li":
+    #         Li.append(candidates)
+    #         LiVotes = len(Li)
+    #     else:
+    #         Otooley.append(candidates)
+    #         OtooleyVotes = len(Otooley)
+    # print(KhanVotes)
+    # print(CorreyVotes)
+    # print(LiVotes)
+    # print(OtooleyVotes)
 
-    # #candidate percentages
-    # KhanPercentage = round((KhanVotes/totalVotes)*100)
-    # print(KhanPercentage)
+    # candidate percentages
+    # KhanPercentage = (KhanVotes/totalVotes)*100
+    # print(f'{KhanPercentage:.3f}%')
 
     
 
 
    #Analysis Summary
-#    analysisSummary = (
-#     f"\nElection Results\n"
-#     f"--------------------\n"
-#     f"Total Votes: {}\n"
-#     f"--------------------\n"
-#     f"Khan: {}\n"
-#     f"Correy: {}\n"
-#     f"Li: {}\n"
-#     f"O'Tooley: {}\n"
-#     f"--------------------\n"
-#     f"Winner: {}\n"
-#     f"--------------------\n")
+analysisSummary = (
+    f"\nElection Results\n"
+    f"--------------------\n"
+    f"Total Votes: {totalVotes}\n"
+    f"--------------------\n"
+    f"'{candidate}: {(votes/totalVotes)*100:.3f}% ({votes})'\n"
+    # f"Khan: {}\n"
+    # f"Correy: {}\n"
+    # f"Li: {}\n"
+    # f"O'Tooley: {}\n"
+    f"--------------------\n"
+    "Winner":[0],
+    f"--------------------\n")
 
-# #print
-# print(analysisSummary)
+#print
+print(analysisSummary)
 
 # #analysisSummary to a text file
 # with open(file_to_analysisSummary, "w")_ as txt_file:
